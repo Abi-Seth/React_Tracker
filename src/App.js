2ecfedb85f4f06400a8e364f7ebf3c62fc722c45
abi-seth <abiheloaf@gmail.com>
2021-11-27 21:43:43 +0200

    New: add states rendered

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { useState } from "react" ;
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 // class based
 // import React from 'react';
 
 function App() {
+	const [showAddTask, setShowAddTask] = useState(false)
 	const [tasks, setTasks] = useState([
         {
             id: 1,
@@ -28,9 +30,17 @@ function App() {
         }
     ])
 
+	//add task
+	const addTask = (task) => {
+		const id = Math.floor(Math.random() * 10000) + 1;
+
+		const newTask = { id, ...task}
+		setTasks([...tasks, newTask])
+	}
+
 	//delete task
 	const deleteTask = (id) => {
-		setTasks(tasks.filter((task) => task.id != id))
+		setTasks(tasks.filter((task) => task.id !== id))
 	}
 
 	//toggle reminder
@@ -40,7 +50,8 @@ function App() {
 
 	return (
 		<div className="container">
-			<Header title="React Tracker" />
+			<Header title="React Tracker" onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+			{showAddTask && <AddTask onAdd={addTask} />}			
 			{ tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No tasks available' }
 		</div>
 	);
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,15 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-const Header = ({ title }) => {
-    const name = 'Seth';
-    const x = true;
-
-    const onClick = () => {
-        alert('Passed throug props to call func!')
-    }
+const Header = ({ title, onAdd, showAdd }) => {
+    // const name = 'Seth';
+    // const x = true;
 
     return (
         <header className='header'>
             <h1 style={{ color: 'black', backgroundColor: 'white'}}>{ title }</h1>
             {/* <p style={paragraphyStyles}>This developer here is { x ? name : 'Unknown' }.</p> */}
-            <Button color='black' title='Add' onClick={onClick}/>
+            <Button color={showAdd ? 'red' : 'black'} title={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
         </header>
     )
 }
@@ -26,9 +22,9 @@ Header.propTypes = {
     title: PropTypes.string.isRequired
 }
 
-const paragraphyStyles = {
-    color: 'blue',
-    fontFamily: 'Fira Code'
-}
+// const paragraphyStyles = {
+//     color: 'blue',
+//     fontFamily: 'Fira Code'
+// }
 
 export default Header
