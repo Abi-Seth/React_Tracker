2b7cb1dac6482988533151bdd7f07f5a96aadfeb
abi-seth <abiheloaf@gmail.com>
2021-11-27 19:39:08 +0200

    Feat: added reminders

--- src/App.js
+++ src/App.js
@@ -28,10 +28,20 @@ function App() {
         }
     ])
 
+	//delete task
+	const deleteTask = (id) => {
+		setTasks(tasks.filter((task) => task.id != id))
+	}
+
+	//toggle reminder
+	const toggleReminder = (id) => {
+		setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+	}
+
 	return (
 		<div className="container">
 			<Header title="React Tracker" />
-			<Tasks tasks={tasks} />
+			{ tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No tasks available' }
 		</div>
 	);
 }
