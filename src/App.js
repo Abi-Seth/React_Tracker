1cb86ac532de6296f50cee673a1713b73e6807b4
abi-seth <abiheloaf@gmail.com>
2021-11-28 21:04:52 +0200

    Feat: changed all static file to backend and dynamic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 import { useState, useEffect } from "react" ;
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
+import About from './components/About';
 
 // class based
 // import React from 'react';
@@ -79,11 +82,21 @@ function App() {
 	}
 
 	return (
-		<div className="container">
-			<Header title="React Tracker" onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
-			{showAddTask && <AddTask onAdd={addTask} />}			
-			{ tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No tasks available' }
-		</div>
+		<Router>
+			<div className="container">
+				<Header title="React Tracker" onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+				<Routes>
+					<Route path='/' exact render={ (props) => (
+						<>
+							{showAddTask && <AddTask onAdd={addTask} />}			
+							{tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No tasks available' }
+						</>)
+					} />
+					<Route path='/about' element={<About />} />
+				</Routes>
+				<Footer />
+			</div>
+		</Router>
 	);
 }
 
